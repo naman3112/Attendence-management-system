fef72735036069868994ffacb78e81934a38e631
Naman Arora <namanarora311@gmail.com>
2023-02-04 12:38:38 +0530

    search functinality implemented

--- boilerplate/app/screens/HomeScreen.tsx
+++ boilerplate/app/screens/HomeScreen.tsx
@@ -1,21 +1,36 @@
 import { observer } from "mobx-react-lite"
-import React, { FC, useEffect } from "react"
-import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import React, { FC, useEffect, useState } from "react"
+import { Image, ImageStyle, TextStyle, View,  useWindowDimensions, Pressable, ViewStyle} from "react-native"
 import {
-  Button, // @demo remove-current-line
+  Button, Icon, // @demo remove-current-line
   Text,
+  TextField
 } from "../components"
 import { isRTL } from "../i18n"
 import { useStores } from "../models" // @demo remove-current-line
 import { AppStackScreenProps } from "../navigators" // @demo remove-current-line
-import { colors, spacing } from "../theme"
+import { colors, spacing ,typography} from "../theme"
 import { useHeader } from "../utils/useHeader" // @demo remove-current-line
 import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
+import {PersonList} from "./Home/PersonList"
+
 
 const welcomeLogo = require("../../assets/images/logo.png")
 const welcomeFace = require("../../assets/images/welcome-face.png")
 
-interface HomeScreenProps extends AppStackScreenProps<"Welcome"> {} // @demo remove-current-line
+interface HomeScreenProps extends AppStackScreenProps<"Welcome"> { } // @demo remove-current-line
+const $customButtonTextStyle: TextStyle = {
+  color: colors.error,
+  fontFamily: typography.primary.bold,
+  textDecorationLine: "underline",
+  textDecorationColor: colors.error,
+  fontSize: 14,
+  paddingVertical: 0
+}
+
+const $customButtonStyle: ViewStyle = {
+paddingVertical: 0,margin: 0 ,minHeight: 26, marginRight: 10}
+
 
 export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen(
   _props, // @demo remove-current-line
@@ -25,16 +40,67 @@ export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen(
   const {
     authenticationStore: { logout, csvData },
   } = useStores()
-useEffect(()=>{
-console.log("csv data is -----", csvData)
-},[])
+  useEffect(() => {
+    //console.log("csv data is -----", csvData)
+  }, [])
   function goNext() {
     navigation.navigate("HomeScreen")
   }
+  const { width} = useWindowDimensions();
+  const [inputText,setInputText] = useState<string>('');
+  const search = () => {
+   
+    return (
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: "center", 
+      marginHorizontal: 10,
+     
+      }}>
+     
+       
+       <TextField
+        label=""
+        helper=""
+        onChangeText={(text)=>{
+          setInputText(text)
+          console.log(text);
+        }}
+       
+   placeholder={"Search here "}
+        //value={inputText}
+        containerStyle={{width: width-100}}
+        inputWrapperStyle={{paddingLeft: 10, justifyContent: 'center', alignItems: 'center'}}
+        LeftAccessory={()=>  <Icon containerStyle={{}} icon={"search"} size={20} /> }
+         RightAccessory={()=> <Button
+          onPress={()=>{
+          
+          }} 
+          preset="filled"
+         style={$customButtonStyle}
+         //textStyle={$customButtonTextStyle}
+         >
+          
+          Search</Button>}
+      />
+      </View>
+
+    )
+
+  }
+
+
 
   useHeader({
-    rightTx: "common.logOut",
-    onRightPress: logout,
+    //rightTx: "common.logOut",
+
+   // onRightPress: logout,
+    LeftActionComponent: search(),
+    //<Icon key={1} icon="ladybug" color={colors.palette.neutral100} size={100} />
+    RightActionComponent: <Text size="sm">Saving...</Text>
+    
+    // LeftActionComponent= {
+    //   <Icon icon={"search"}/>
+    // }
+    //,
   })
   // @demo remove-block-end
 
@@ -42,36 +108,20 @@ console.log("csv data is -----", csvData)
 
   return (
     <View style={$container}>
-      <View style={$topContainer}>
-        <Text
-          testID="welcome-heading"
-          style={$welcomeHeading}
-          tx="welcomeScreen.readyForLaunch"
-          preset="heading"
-        />
-        <Text tx="welcomeScreen.exciting" preset="subheading" />
-      </View>
 
-      <View style={[$bottomContainer, $bottomContainerInsets]}>
-        <Text tx="welcomeScreen.postscript" size="md" />
-        {/* @demo remove-block-start */}
-        <Button
-          testID="next-screen-button"
-          preset="reversed"
-          tx="welcomeScreen.letsGo"
-          onPress={goNext}
-        />
-        {/* @demo remove-block-end */}
-      </View>
+    <PersonList
+    inputText={inputText}
+    />
+
     </View>
   )
 })
 
 const $container: ViewStyle = {
+  paddingTop: 16, 
   flex: 1,
   backgroundColor: colors.background,
 }
-
 const $topContainer: ViewStyle = {
   flexShrink: 1,
   flexGrow: 1,
